cdebc5f355bc8fcddaf53561465037b34447882c
Roman <hush9162@gmail.com>
2023-06-01 21:49:21 +0300

    Добавлена генерация списка навыков через javascript

---
+++ scripts/script.js
@@ -0,0 +1,34 @@
+const skills = {
+    data: [
+    { name: 'html', level: 30, class: 'skill-item skill-item_html', icon: 'html.svg'},
+    { name: 'css', level: 40, class: 'skill-item skill-item_css', icon: 'css.svg'},
+    { name: 'python', level: 10, class: 'skill-item skill-item_python',  icon: 'python.svg'},
+    { name: 'cpp', level: 70, class: 'skill-item skill-item_cpp', icon: 'cpp.svg'},
+    ],
+
+    generateList: function(parentElement) {
+        this.data.forEach(skill => {
+        const skillItem = document.createElement('dt');
+        const skillLevel = document.createElement('dd');
+        const skillPercent = document.createElement('div');
+        const iconPath = `../img/${skill.icon}`;
+
+        skillItem.style.backgroundImage = `url(${iconPath})`;
+
+        skillItem.classList.add('skill-item');
+        skillLevel.classList.add('skill-level');
+        skillPercent.classList.add(skill.cssClass);
+
+        skillItem.textContent = skill.name;
+        skillLevel.textContent = `${skill.level}%`;
+
+        skillPercent.style.width = `${skill.level}%`;
+        skillLevel.appendChild(skillPercent);
+
+        skillList.appendChild(skillItem);
+        skillList.appendChild(skillLevel);
+    });
+}
+};
+const skillList = document.querySelector('dl.skill-list');
+skills.generateList(skillList);
