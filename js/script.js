d4ce39e1ccdc9803dfbb78d2fca9455470fa5a0d
Roman <hush9162@gmail.com>
2023-06-06 18:01:03 +0300

    2-я часть готова

--- js/script.js
+++ js/script.js
@@ -1,63 +1,60 @@
 const skills = {
-    data: [
-      { name: 'html', level: 30, icon: 'html.svg' },
-      { name: 'css', level: 40, icon: 'css.svg' },
-      { name: 'python', level: 10, icon: 'python.svg' },
-      { name: 'cpp', level: 70, icon: 'cpp.svg' },
-    ],
+  data: [
+    { name: 'html', level: 30, icon: 'html.svg' },
+    { name: 'css', level: 40, icon: 'css.svg' },
+    { name: 'python', level: 10, icon: 'python.svg' },
+    { name: 'cpp', level: 70, icon: 'cpp.svg' },
+  ],
+
+  generateList: function(parentElement) {
+    parentElement.innerHTML = '';
+    this.data.forEach(skill => {
+      const skillItem = document.createElement('dt');
+      const skillLevel = document.createElement('dd');
+      const skillPercent = document.createElement('div');
+      const iconPath = `img/${skill.icon}`;
+      skillItem.style.backgroundImage = `url(${iconPath})`;
+      skillItem.classList.add('skill-item');
+      skillLevel.classList.add('skill-level');
+      skillItem.textContent = skill.name;
+      skillPercent.style.width = `${skill.level}%`;
+      skillPercent.textContent = `${skill.level}%`;
+      skillLevel.appendChild(skillPercent);
+      parentElement.appendChild(skillItem);
+      parentElement.appendChild(skillLevel);
+    });
+  },
+
+  sortList: function(prop) {
+    const isDescendingOrder = (prop === this.sortProperty) ? !this.isDescendingOrder : false;
+    this.sortProperty = prop;
+    this.isDescendingOrder = isDescendingOrder;
   
-    generateList: function(parentElement) {
-      parentElement.innerHTML = '';
-      this.data.forEach(skill => {
-        const skillItem = document.createElement('dt');
-        const skillLevel = document.createElement('dd');
-        const skillPercent = document.createElement('div');
+    this.data.sort(this.getComparer(prop));
+    if (isDescendingOrder) {
+      this.data.reverse();
+    }
   
-        const iconPath = `img/${skill.icon}`;
-        skillItem.style.backgroundImage = `url(${iconPath})`;
+    this.generateList(skillList);
+  },
   
-        skillItem.classList.add('skill-item');
-        skillLevel.classList.add('skill-level');
-  
-        skillItem.textContent = skill.name;
-        skillPercent.style.width = `${skill.level}%`;
-        skillPercent.textContent = `${skill.level}%`;
-  
-        skillLevel.appendChild(skillPercent);
-        parentElement.appendChild(skillItem);
-        parentElement.appendChild(skillLevel);
-      });
-    },
-  
-    sortList: function(prop) {
-      const isDescendingOrder = (prop === this.sortProperty) ? !this.isDescendingOrder : false;
-      this.sortProperty = prop;
-      this.isDescendingOrder = isDescendingOrder;
-  
-      this.data.sort(this.getComparer(prop));
-      if (isDescendingOrder) {
-        this.data.reverse();
+  getComparer: function(prop) {
+    return function(a, b) {
+      switch (prop) {
+        case 'name':
+          return a[prop].localeCompare(b[prop]);
+        case 'level':
+          return a[prop] - b[prop];
+        default:
+          return 0;
       }
+    };
+  },
   
-      this.generateList(skillList);
-    },
-  
-    getComparer: function(prop) {
-      return function(a, b) {
-        switch (prop) {
-          case 'name':
-            return a[prop].localeCompare(b[prop]);
-          case 'level':
-            return a[prop] - b[prop];
-          default:
-            return 0;
-        }
-      };
-    },
-  
-    sortProperty: '', 
-    isDescendingOrder: false,
-  };
+  sortProperty: '', 
+  isDescendingOrder: false,
+};
+
 const skillList = document.querySelector('dl.skill-list');
 skills.generateList(skillList);
 
@@ -96,4 +93,33 @@ function getComparer(prop) {
   
       return 0;
     };
+}; 
+
+const menu = {
+  navMenu: document.querySelector('.main-nav'),
+  navButton: document.querySelector('.nav-btn'),
+
+  open: function() {
+    this.navMenu.classList.remove('main-nav_closed');
+    this.navButton.classList.remove('nav-btn_open');
+    this.navButton.classList.add('nav-btn_close');
+    this.navButton.innerHTML = 
+    '<span class="visually-hidden">Открыть меню</span>'
+  },
+
+  close: function() {
+    this.navMenu.classList.add('main-nav_closed');
+    this.navButton.classList.remove('nav-btn_close');
+    this.navButton.classList.add('nav-btn_open');
+    this.navButton.innerHTML = 
+    '<span class="visually-hidden">Закрыть меню</span>'
   }
+};
+
+menu.navButton.addEventListener('click', () => {
+  if (menu.navMenu.classList.contains('main-nav_closed')) {
+    menu.open();
+  } else {
+    menu.close();
+  }
+});
